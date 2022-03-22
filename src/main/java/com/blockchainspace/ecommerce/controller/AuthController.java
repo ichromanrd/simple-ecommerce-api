ba971f99d48f491d0f5afc32ae209dd0212debbc
Ichroman Raditya Duwila <ichromanraditya.duwila@gmail.com>
2022-03-22 17:24:52 +0900

    remove unused methods

--- src/main/java/com/blockchainspace/ecommerce/controller/AuthController.java
+++ src/main/java/com/blockchainspace/ecommerce/controller/AuthController.java
@@ -1,37 +1,20 @@
 package com.blockchainspace.ecommerce.controller;
 
 import com.blockchainspace.ecommerce.dto.request.AuthRequest;
-import com.blockchainspace.ecommerce.dto.request.InvalidateSessionRequest;
-import com.blockchainspace.ecommerce.dto.request.RefreshTokenRequest;
 import com.blockchainspace.ecommerce.dto.response.AuthResponse;
-import com.blockchainspace.ecommerce.dto.response.UserResponse;
-import com.blockchainspace.ecommerce.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-
-    @Autowired
-    private UserService userService;
 
     @PostMapping("/authenticate")
     public AuthResponse authenticate(@RequestBody AuthRequest request) {
         // TODO implement service
         return null;
-    }
-
-    @GetMapping
-    public List<UserResponse> getUserList() {
-        return userService.getUserList();
     }
 
 }
